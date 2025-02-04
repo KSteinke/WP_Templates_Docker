30a572334441b7d88b7df59a9e6c07209bc1a830
KSteinke <kacpersteinkesb@gmail.com>
2025-02-04 23:03:59 +0100

    style.css connected to front page

--- wp-content/wp-content/themes/MyFirstTemplate/front-page.php
+++ wp-content/wp-content/themes/MyFirstTemplate/front-page.php
@@ -1,3 +1,15 @@
-<!-- this is a landing page site -->
-
-<h1> Hello World! <h1>
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title><?php wp_title(); ?></title>
+    <?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+    <!-- this is a landing page site -->
+    <h1>Hello World!</h1>
+    <div class="test">wielki teks</div>
+    <?php wp_footer(); ?>
+</body>
+</html>
