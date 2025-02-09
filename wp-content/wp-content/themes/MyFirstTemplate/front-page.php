04f3b51fcee6aaf6468a73592ec155a80b91855a
KSteinke <kacpersteinkesb@gmail.com>
2025-02-09 10:51:37 +0100

    front-page skelet

--- wp-content/wp-content/themes/MyFirstTemplate/front-page.php
+++ wp-content/wp-content/themes/MyFirstTemplate/front-page.php
@@ -1,16 +1,18 @@
-<!DOCTYPE html>
-<html <?php language_attributes(); ?>>
-<head>
-    <meta charset="<?php bloginfo( 'charset' ); ?>">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title><?php wp_title(); ?></title>
-    <?php wp_head(); ?>
-</head>
-<body <?php body_class(); ?>>
+
+<?php get_header(); ?>
+<body>
     <!-- this is a landing page site -->
-    <h1>Hello World!</h1>
+    <?php 
+    if ( have_posts() ) {
+        while ( have_posts() ) 
+        {
+            the_post();
+            the_content();
+        }
+    }
+    ?>
     
 </body>
-
-<?php wp_footer(); ?>
+<?php wp_head(); ?>
+<?php get_footer(); ?>
 </html>
